f88d6cf9dff15ace2e417c21b206f09f5ababe8c
thiago-costa-dev <costathiagocontat@gmail.com>
2021-12-12 14:35:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Site Block Extension
+
+A extension to block acession to some pages.
---
+++ index.js
@@ -0,0 +1,19 @@
+function main() {
+    const currentUrl = window.location.href;
+    const bannedUrls = [
+        'youtube',
+        'facebook',
+        'twitter',
+        'instagram',
+        'imdb'
+    ];
+    const redirectUrl = 'https://c.tenor.com/657Vy3stnf8AAAAd/away-para-com-essa-porra.gif';
+
+    bannedUrls.forEach(url => {
+        if (currentUrl.search(url) !== -1) {
+            window.location.replace(redirectUrl);
+        }
+    });
+}
+
+main();
